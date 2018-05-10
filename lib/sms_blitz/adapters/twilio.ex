ff1a76811feb78899b2af3fb9dd9284b5db873f7
Lee McLaughlin <lee@icmobilelab.com>
2018-05-10 15:06:55 +0100

    Fix auth in twilio adapter

--- lib/sms_blitz/adapters/twilio.ex
+++ lib/sms_blitz/adapters/twilio.ex
@@ -1,20 +1,24 @@
 defmodule SmsBlitz.Adapters.Twilio do
   @base_uri "https://api.twilio.com/2010-04-01/Accounts"
 
-  def authenticate({account_sid}) do
+  def authenticate({account_sid, token}) do
     %{
-      uri: Enum.join([@base_uri, account_sid, "Messages.json"], "/")
+      uri: Enum.join([@base_uri, account_sid, "Messages"], "/"),
+      auth: [hackney: [basic_auth: {account_sid, token}]]
     }
   end
 
-  def send_sms(%{uri: uri}, from: from, to: to, message: message) when is_binary(from) and is_binary(to) and is_binary(message) do
+  def send_sms(%{uri: uri, auth: auth}, from: from, to: to, message: message) when is_binary(from) and is_binary(to) and is_binary(message) do
     params = [
       To: to,
       From: from
     ]
+    auth = 
     {:ok, %HTTPoison.Response{body: resp, status_code: status_code}} = HTTPoison.post(
       uri,
-      {:form, params}
+      {:form, params},
+      %{},
+      auth
     )
     {:ok, resp_json} = Poison.decode(resp)
 
