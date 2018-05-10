e4c732e0938451efe65e229a0c1641f1d97ea963
Lee McLaughlin <lee@icmobilelab.com>
2018-05-10 16:10:56 +0100

    fixed twilio adapter

--- lib/sms_blitz/adapters/twilio.ex
+++ lib/sms_blitz/adapters/twilio.ex
@@ -1,24 +1,29 @@
 defmodule SmsBlitz.Adapters.Twilio do
+  require Logger
   @base_uri "https://api.twilio.com/2010-04-01/Accounts"
 
   def authenticate({account_sid, token}) do
     %{
-      uri: Enum.join([@base_uri, account_sid, "Messages"], "/"),
-      auth: [hackney: [basic_auth: {account_sid, token}]]
+      uri: Enum.join([@base_uri, account_sid, "Messages.json"], "/"),
+      auth: Base.encode64("#{account_sid}:#{token}")
     }
   end
 
   def send_sms(%{uri: uri, auth: auth}, from: from, to: to, message: message) when is_binary(from) and is_binary(to) and is_binary(message) do
+
     params = [
       To: to,
-      From: from
+      From: from,
+      Body: message
     ]
     {:ok, %HTTPoison.Response{body: resp, status_code: status_code}} = HTTPoison.post(
       uri,
       {:form, params},
-      %{},
-      auth
-    )
+      [
+        {"Authorization", "Basic " <> auth}
+      ]
+      )
+
     {:ok, resp_json} = Poison.decode(resp)
 
     %{
--- test/sms_blitz_test.exs
+++ test/sms_blitz_test.exs
@@ -5,4 +5,16 @@ defmodule SmsBlitzTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "send twilio" do
+    # Attempt to send the sms
+    sms_receipt =
+      SmsBlitz.send_sms(
+        :twilio,
+        from: "FAP",
+        to: "+447792638416",
+        message: "hi message"
+      )
+
+  end
 end
